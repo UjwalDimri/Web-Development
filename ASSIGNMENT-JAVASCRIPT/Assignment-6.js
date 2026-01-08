0a9a706934079c98cdeb2731e6292ee945a4e105
Ujwal dimri <ujwaldimri223@gmail.com>
2026-01-08 23:11:18 +0530

    practicing js

---
+++ ASSIGNMENT-JAVASCRIPT/Assignment-6.js
@@ -0,0 +1,15 @@
+// Practice Question
+// create a function which prints a poem 
+function poem() {
+    console.log(`Twinkle, twinkle, little star,\n How I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky. `);
+}
+
+poem();
+
+// practice question
+// create a fuction for dice roll
+function diceRoll() {
+    let dice = Maths.floor(Maths.random()*6)+1;
+    console.log(dice);
+}
+diceRoll();
