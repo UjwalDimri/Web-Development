c095d29b8363493df047c13834776748c67ae094
Ujwal dimri <ujwaldimri223@gmail.com>
2026-01-10 22:34:42 +0530

    PRACTICING FUNCTIONS

---
+++ ASSIGNMENT-JAVASCRIPT/Assignment-7.js
@@ -0,0 +1,34 @@
+// PRACTICE QUESTION
+// Write an arrow function that return the square of a number 'n'
+let square = (n) => (
+    n*n
+);
+square(10);
+
+// PRACTICE QUESTION
+// Write a function that prints "Hello World" 5 times at intervals of 2 sec 
+let id = setInterval(() => {
+     console.log("Hello World");
+}, 2000);
+
+let hw = setTimeout(() => {
+    clearInterval(id);
+}, 10000);
+
+
+// Assignment - 7 
+console.log("Assignment - 7");
+// Qs1 . Write an arrow function named array Average that accepts an array of numbers and returns the average of those numbers.
+
+const Average = (array) => {
+    let sum = 0;
+    for(let i = 0;i<array.length;i++){
+        sum = sum+array[i];
+    }
+    let avg = (sum)/array.length;
+    console.log(avg);
+    return avg;
+}
+ 
+let array = [1,2,3,4,5,6,7,8,9,10];
+Average(array);
