640fbdad07486669fa0acae7c11f69722982c30e
UjwalDimri <ujwaldimri223@gmail.com>
2026-06-06 22:49:20 +0530

    Connect SQL with NODE

---
+++ Backend/SQL_CLASS/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let getUser = () =>{
+//     return {
+//         id : faker.string.uuid(),
+//         username : faker.internet.username(),
+//         email : faker.internet.email(),
+//         password : faker.internet.password(),
+
+//     };
+// };
+// console.log(getUser());
